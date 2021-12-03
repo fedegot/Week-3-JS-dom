2e70ba55eac535f02b77f3ce017b001e4d81fbc8
Federico Gottardi <gotfede@gmail.com>
2021-12-03 13:57:48 +0000

    week 3.1

--- challenges/dice/script.js
+++ challenges/dice/script.js
@@ -1,10 +1,19 @@
-const roll = document.document.getElementById("roll");
-const result = document.getElementsById("gamearea");
+let list = [];
+let sum = 0;
+let button = document.getElementById("roll");
+let write = document.getElementById("gamearea");
+const pic = document.getElementById("dice1")
 
+button.onclick=function (){
+pic.style.background = "url('./pic/di.jpg') no-repeat center center";
+let score = Math.floor(Math.random() * 6);
+list.push(score);
+sum += score;
 
+write.innerHTML = sum;
 
-let score = 0;
-
-document.document.getElementById("roll").addEventListener('click', function () {
-    score = Math.floor(Math.random() * 20);
-    document.getElementsById("gamearea").innerHTML=(`total score ${score}`)};
+if (sum >= 20){
+write.innerHTML = (`congratulation last dice: ${score} and your total score is: ${sum}`), sum =0, list.length = 0;}
+else if (score <= 1){
+write.innerHTML = (`your last dice was: ${score} game over `), sum =0, list.length = 0;}
+}
